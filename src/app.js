fbd5f44fe730c342372d6a723a71785d1c2fd0b3
LOGEDI <nelsonlogedi@gmail.com>
2022-11-18 12:27:38 +0000

    final work

--- src/app.js
+++ src/app.js
@@ -5,39 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  document.querySelector("#then.excuse").innerHTML = generateExcuse();
-  console.log("Hello Rigo from the console!");
-};
-
 let generateExcuse = () => {
   let pronoun = ["A", "The"];
-  let subject = ["driver", "dog", "roocon", "footballer", "commedian"];
-  let action = ["took my", "ate my", "ride my", "stole my", "yelled at"];
-  let possesion = ["homework", "toe", "shoes", "car", "book"];
-  let where = [
-    "on my street",
-    "in my room",
-    "at my drive way",
-    "im my book",
-    "at my office"
-  ];
+  let subject = ["jogger", "dog", "raocon", "driver", "picone"];
+  let action = ["took my", "threw away", "ride my bike"];
+  let possesion = ["homework", "shoes", "car", "book"];
+  let where = ["in my room", "on the streat", "at my office"];
 
-  let proIndex = math.floor(math.random() * pronoun.length);
-  let subjIndex = math.floor(math.random() * subject.length);
-  let actionIndex = math.floor(math.random() * action.length);
-  let possesionIndex = math.floor(math.random() * possesion.length);
-  let whereIndex = math.floor(math.random() * where.length);
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subjIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possesionIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
 
   return (
     pronoun[proIndex] +
-    "" +
+    " " +
     subject[subjIndex] +
-    "" +
-    actionIndex[actionIndex] +
-    "" +
+    " " +
+    action[actionIndex] +
+    "  " +
     possesion[possesionIndex] +
-    "" +
+    " " +
     where[whereIndex]
   );
+};
+console.log(generateExcuse());
+
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#then_excuse").innerHTML = generateExcuse();
+  });
 };
